d6b9260702fdc74e1e01a8e4f7b9c547f79a4fbc
stephen042 <makuochukwu042@gmail.com>
2024-06-09 22:02:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="public/assets/img/favicon.png" rel="icon">
+    <link href="public/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="public/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="public/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="public/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="public/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="public/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="public/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/assets/css/custom.css">
+
+    <!-- Template Main CSS File -->
+    <link href="public/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">Home</a>
+                <button class="navbar-toggler" style="background-color:grey;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="ri-align-justify"></span>
+
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="public/auth/login.php">Login</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <div class="hero">
+            <div id="carouselExampleIndicators" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./public/assets/img/slides-1.jpg" class="d-block w-100" width="100" height="590" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./public/assets/img/slides-2.jpg" class="d-block w-100" width="100" height="590" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./public/assets/img/slides-3.jpg" class="d-block w-100" width="100" height="590" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <!-- Vendor JS Files -->
+    <script src="public/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="public/assets/vendor/chart.js/chart.min.js"></script>
+    <script src="public/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="public/assets/vendor/quill/quill.min.js"></script>
+    <script src="public/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="public/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="public/assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="public/assets/js/main.js"></script>
+
+</body>
+
+</html>
